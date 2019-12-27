e5541f0eee8b378bdce759083a4eacbef4aba113
Salman Alamdar <salmanbinhilabi@gmail.com>
2019-12-27 19:03:41 +0530

    Updates

---
+++ precache-manifest.14bd2ef510f7e9ceabc0a372211b8b37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860f076dde4e2626b9300f23daa1b755",
+    "url": "/React-Redux-Blog/index.html"
+  },
+  {
+    "revision": "c12d30d56d2cba7b4231",
+    "url": "/React-Redux-Blog/static/css/main.2522c16d.chunk.css"
+  },
+  {
+    "revision": "83cac635dd580e6c7309",
+    "url": "/React-Redux-Blog/static/js/2.2b68cf09.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/React-Redux-Blog/static/js/2.2b68cf09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c12d30d56d2cba7b4231",
+    "url": "/React-Redux-Blog/static/js/main.330b24b5.chunk.js"
+  },
+  {
+    "revision": "c039cdc0d4d2526b8c00",
+    "url": "/React-Redux-Blog/static/js/runtime-main.8cdc4240.js"
+  }
+]);
